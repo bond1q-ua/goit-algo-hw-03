3658130fc62b2bec23d30551e39e5898537febc4
bond1q-ua <157967764+bond1q-ua@users.noreply.github.com>
2024-03-11 22:13:04 +0200

    uodate2

--- task_1.py
+++ task_1.py
@@ -1,1 +1,10 @@
-print('Hellow World!')
+from datetime import datetime
+def get_days_from_today(date):
+    date_obj = datetime.strptime(date, '%Y-%m-%d')
+    today = datetime.today()
+    delta = today - date_obj
+    return delta.days
+
+date = '2022-02-24'
+days_difference = get_days_from_today(date)
+print("Кількість днів з {} (почтаку повномаштабного вторгення) і сьогодні: {}".format(date, days_difference))
