36d6cac736e7c28cdbfbd3f407d22c123b0e02a5
bond1q-ua <157967764+bond1q-ua@users.noreply.github.com>
2024-03-11 22:56:53 +0200

    complited 1

--- task_2.py
+++ task_2.py
@@ -1,1 +1,15 @@
-print('Hellow World!')
+import random
+def get_numbers_ticket(min_num, max_num, quantity):
+    if min_num < 1 or max_num > 1000 or quantity < 1 or quantity > (max_num - min_num + 1):
+        return []
+
+     numbers_set = set()
+    while len(numbers_set) < quantity:
+        numbers_set.add(random.randint(min_num, max_num))
+    return sorted(numbers_set)
+
+min_num = 1
+max_num = 49
+quantity = 6
+lottery_numbers = get_numbers_ticket(min_num, max_num, quantity)
+print("Унікальні числа для лотерейного квитка:", lottery_numbers)
