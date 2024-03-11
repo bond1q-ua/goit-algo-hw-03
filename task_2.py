3b9b19ffc5d634de66753f92e624fc96ab118d0c
bond1q-ua <157967764+bond1q-ua@users.noreply.github.com>
2024-03-11 23:32:26 +0200

    I hope it correctly

--- task_2.py
+++ task_2.py
@@ -3,7 +3,7 @@ def get_numbers_ticket(min_num, max_num, quantity):
     if min_num < 1 or max_num > 1000 or quantity < 1 or quantity > (max_num - min_num + 1):
         return []
 
-     numbers_set = set()
+    numbers_set = set()
     while len(numbers_set) < quantity:
         numbers_set.add(random.randint(min_num, max_num))
     return sorted(numbers_set)
